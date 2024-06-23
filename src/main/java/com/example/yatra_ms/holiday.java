8068b3f3e631dd76acac726750112e9b5a6eb55d
“Satyam <satyam@gmail.com>
2024-06-23 09:16:46 +0530

    changes in pom.xml

--- src/main/java/com/example/yatra_ms/holiday.java
+++ src/main/java/com/example/yatra_ms/holiday.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holiday {
     @GetMapping("/holiday")
-    public String getData() {return  "Please book your holidays ticket from yatra app on  40% discount" ; }
+    public String getData() {return  "Please book your holidays ticket from yatra app on  10% discount" ; }
 }
