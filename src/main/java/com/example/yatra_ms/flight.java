d0f884aac82ce57067d1d72ffc156848b45d87ba
satyam88 <satyampandey88@gmail.com>
2024-06-29 09:35:33 +0530

    Update flight.java

--- src/main/java/com/example/yatra_ms/flight.java
+++ src/main/java/com/example/yatra_ms/flight.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flight {
     @GetMapping("/flight")
-    public String getData() {return  "Please book your flight ticket from yatra app on  40% discount" ; }
+    public String getData() {return  "Please book your flight ticket from yatra app on  100% discount" ; }
 }
